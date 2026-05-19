6d67cc9430ff619bf9944bd45cae81e8a9074c9b
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-05-19 17:01:53 +0200

    Add

--- Pawmunity/src/router/index.js
+++ Pawmunity/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FicheAnimalView from "../views/FicheAnimalView.vue";
+import CalendarView from "../views/CalendarView.vue";
+import ProfileView from "../views/ProfileView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/calendrier',
+      name: 'calendar',
+      component: CalendarView ,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView ,
+    },
+    {
+      path: '/ficheAnimal',
+      name: 'ficheAnimal',
+      component: FicheAnimalView ,
     },
   ],
 })
