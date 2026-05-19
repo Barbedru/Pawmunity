8d274434c7a99a61deae504bb323c69ca7c9c59d
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-05-19 17:00:06 +0200

    Ajout components

---
+++ Pawmunity/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="flex items-center justify-between px-8 py-4 bg-white ">
+
+    <img src="@/assets/logoPawmunity.png" alt="Logo_Pawmunity" class="h-30" />
+
+    <div class="flex gap-8">
+      <router-link to="/" class="text-gray-600 hover:text-gray-900">Accueil</router-link>
+      <router-link to="/calendrier" class="text-gray-600 hover:text-gray-900">Calendrier</router-link>
+      <router-link to="/profile" class="text-gray-600 hover:text-gray-900">Profile</router-link>
+
+    </div>
+
+
+      <div class="cursor-pointer">
+        <img src="@/assets/bell.png" alt="icon_bell" />
+      </div>
+
+
+  </nav>
+
+</template>
+
+<style scoped>
+
+</style>
