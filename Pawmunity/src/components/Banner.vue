d3bd4296bad94088b0a8ddd655788dedd01d2d94
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-05-26 16:07:36 +0200

    Component Banner

---
+++ Pawmunity/src/components/Banner.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="w-full bg-white flex items-center justify-center border-b border-gray-200"
+       style="height: 141px; padding-top: 50px;">
+    <img
+      src="../assets/logoPawmunity.png"
+      alt="Pawmunity"
+      class="w-90"
+    />
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
