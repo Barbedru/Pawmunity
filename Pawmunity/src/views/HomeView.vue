f14cc7e234d230c647bbbeae5c367c51eca1d781
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-05-28 15:15:05 +0200

    création HomeView

--- Pawmunity/src/views/HomeView.vue
+++ Pawmunity/src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup>
 
+import { useRouter } from 'vue-router'
+import Banner from '../components/Banner.vue'
+const router = useRouter()
+
 </script>
 
 <template>
+  <div class="min-h-screen bg-[#F5F1EB] flex flex-col">
+    <Banner />
+
+  </div>
+
 
 </template>
 
