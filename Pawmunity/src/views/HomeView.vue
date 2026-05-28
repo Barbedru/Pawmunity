47c1886c6f0183a197df8d56f9b5560ac0d19615
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-05-28 16:15:34 +0200

    intégration component MenuBurger

--- Pawmunity/src/views/HomeView.vue
+++ Pawmunity/src/views/HomeView.vue
@@ -2,13 +2,16 @@
 
 import { useRouter } from 'vue-router'
 import Banner from '../components/Banner.vue'
-const router = useRouter()
+import MenuBurger from '../components/MenuBurger.vue'
+
 
 </script>
 
 <template>
   <div class="min-h-screen bg-[#F5F1EB] flex flex-col">
     <Banner />
+    <MenuBurger />
+
 
   </div>
 
