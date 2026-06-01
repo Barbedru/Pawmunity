27a2f4217d417eaf60f3c04277786284b3af6672
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-01 16:29:05 +0200

    Ajout donnée mock

---
+++ Pawmunity/src/data/demandes.js
@@ -0,0 +1,44 @@
+import {ref} from "vue";
+
+export const demandes = [
+  {
+    id: 1,
+    nom: "Murphy",
+    famille: "Famille Druval",
+    description: "Besoin urgent de garde pour Murphy. Départ imprévu ce soir",
+    date: "29-30 Juin",
+    lieu: true,
+    reponses: 2,
+    urgent: true
+  },
+  {
+    id: 2,
+    nom: "Suzie",
+    famille: "Famille Clet",
+    description: "J'aurais besoin que quelqu'un s'occupe de Suzie entre les midis",
+    date: "30 Juin",
+    lieu: false,
+    reponses: 1,
+    urgent: true
+  },
+  {
+    id: 3,
+    nom: "Zouk",
+    famille: "Famille Miquet",
+    description: "J'ai besoin qu'on s'occupe de Zouk pour le week-end",
+    date: "1-2 Août",
+    lieu: false,
+    reponses: 1,
+    urgent: false
+  },
+  {
+    id: 4,
+    nom: "Leito & Arizona",
+    famille: "Famille Pastori",
+    description: "J'ai besoin qu'on s'occupe de Zouk pour 2 jours",
+    date: "5-6 Août",
+    lieu: false,
+    reponses: 2,
+    urgent: false
+  },
+]
