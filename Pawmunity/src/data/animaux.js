bf1f4c739dfad3b047a8cc44a7bbd8686e1f4933
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-02 15:35:03 +0200

    ajout bdd animaux

---
+++ Pawmunity/src/data/animaux.js
@@ -0,0 +1,20 @@
+export const animaux = [
+  {
+    id: 1,
+    nom: "Murphy",
+    type: "Chien",
+    age: 4
+  },
+  {
+    id: 2,
+    nom: "Odin",
+    type: "Chat",
+    age: 8
+  },
+  {
+    id: 3,
+    nom: "Einar",
+    type: "Chat",
+    age: 8
+  }
+]
