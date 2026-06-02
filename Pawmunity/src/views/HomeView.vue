680b16f32883375d28be0482ec90447577cbf244
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-02 15:36:05 +0200

    Ajout form demande

--- Pawmunity/src/views/HomeView.vue
+++ Pawmunity/src/views/HomeView.vue
@@ -2,10 +2,10 @@
 
 import { ref, computed } from 'vue'
 import { demandes as demandesData } from '../data/demandes.js'
-import { useRouter } from 'vue-router'
 import Banner from '../components/Banner.vue'
 import MenuBurger from '../components/MenuBurger.vue'
 import CarteDemande from '../components/CarteDemande.vue'
+import FormulaireDemande from '../components/FormulaireDemande.vue'
 
 const filtreActif = ref('toutes')
 
@@ -17,6 +17,11 @@ const demandesFiltrees = computed(() => {
 })
 
 const demandes = ref(demandesData)
+const afficherFormulaire = ref(false)
+
+const ajouterDemande = (nouvelleDemande) => {
+  demandes.value.push(nouvelleDemande)
+}
 
 </script>
 
@@ -31,6 +36,7 @@ const demandes = ref(demandesData)
 
     <button
       @click="filtreActif = 'toutes'"
+      :class="filtreActif === 'toutes' ? 'shadow-[0_4px_10px_rgba(44,74,110,0.4)]' : ''"
       class="w-[90%] mx-auto bg-[#2C4A6E] text-white font-bold h-15.5 rounded-full mb-4 text-2xl"
     >
       Toutes les demandes
@@ -38,14 +44,14 @@ const demandes = ref(demandesData)
 
     <button
       @click="filtreActif = 'urgentes'"
-      class="w-[60%] mx-auto bg-white text-[#2C4A6E] font-bold h-15.5 rounded-full mb-4 text-2xl shadow-[0_4px_10px_rgba(44,74,110,0.4)] hover:shadow-[0_6px_16px_rgba(44,74,110,0.5)]
-       active:shadow-none transition-shadow duration-150"
+      :class="filtreActif === 'urgentes' ? '' : 'shadow-[0_4px_10px_rgba(44,74,110,0.4)]'"
+      class="w-[60%] mx-auto bg-white text-[#2C4A6E] font-bold h-15.5 rounded-full mb-4 text-2xl transition-shadow duration-150"
     >
       Urgentes
     </button>
 
     <button
-      @click=""
+      @click="afficherFormulaire = true"
       class="w-[90%] mx-auto bg-[#FF7A4D] text-white font-bold h-15.5 rounded-xl mb-4 text-2xl shadow-[0_4px_10px_rgba(44,74,110,0.4)] hover:shadow-[0_6px_16px_rgba(44,74,110,0.5)]
        active:shadow-none transition-shadow duration-150"
     >
@@ -65,8 +71,14 @@ const demandes = ref(demandesData)
     />
 
 
+  <FormulaireDemande
+    v-if="afficherFormulaire"
+    @fermer="afficherFormulaire = false"
+    @ajouterDemande="ajouterDemande"
+  />
 
   </div>
+
 
 
 </template>
