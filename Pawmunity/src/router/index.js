bae920987f3d102408fbb795146eaa623aedb651
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-02 15:36:18 +0200

    rien de spécial

--- Pawmunity/src/router/index.js
+++ Pawmunity/src/router/index.js
@@ -6,6 +6,7 @@ import HomeView from '../views/HomeView.vue'
 import CalendarView from '../views/CalendarView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import FicheAnimalView from '../views/FicheAnimalView.vue'
+import FormulaireDemande from "../components/FormulaireDemande.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
