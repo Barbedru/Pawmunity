a729ce823a8a8db7f6d5790739db8e94cb69cf5c
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-05 13:59:10 +0200

    format code

--- Pawmunity/src/components/ProfilBanner.vue
+++ Pawmunity/src/components/ProfilBanner.vue
@@ -17,20 +17,21 @@ const initiales = computed( ()  => {
 
   <div class="w-full h-[233px] -b-3xl flex items-center px-6 gap-6 "
        style="background: linear-gradient(180deg, #2C4A6E, #E8724A)">
-    <!-- Avatar-->
+    <!--Avatar-->
     <div class="w-[109px] h-[111px] rounded-full flex items-center justify-center font-light text-[40px] text-white"
          style="background: linear-gradient(180deg, #E8724A, #2C4A6E)">
       {{ initiales }}
     </div>
 
-    <!-- Nom Famille-->
+    <!--Nom Famille-->
     <div>
-      <p class="font-light text-white text-[40px]"> famille</p>
-      <p class="font-light text-white text-[40px]"> {{nom}}</p>
+      <p class="font-light text-white text-[40px]"> Famille </p>
+      <p class="font-light text-white text-[40px]"> {{nom}} </p>
     </div>
 
-
   </div>
+
+
 </template>
 
 <style scoped>
