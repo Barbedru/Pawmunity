1776b955693cff061136e168ebf45ab2f048454e
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-05 16:43:16 +0200

    modif

--- Pawmunity/src/data/animaux.js
+++ Pawmunity/src/data/animaux.js
@@ -1,20 +1,5 @@
 export const animaux = [
-  {
-    id: 1,
-    nom: "Murphy",
-    type: "Chien",
-    age: 4
-  },
-  {
-    id: 2,
-    nom: "Odin",
-    type: "Chat",
-    age: 8
-  },
-  {
-    id: 3,
-    nom: "Einar",
-    type: "Chat",
-    age: 8
-  }
-]
+  { id: 1, nom: "Murphy", type: "Chien", age: 4, emoji: "🐶" },
+  { id: 2, nom: "Odin", type: "Chat", age: 8, emoji: "🐱" },
+  { id: 3, nom: "Einar", type: "Chat", age: 8, emoji: "🐱" },
+  ]
