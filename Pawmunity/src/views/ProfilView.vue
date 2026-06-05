a583daf4c9b1a3e7a121e751e13fa860267b9ef7
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-05 13:46:25 +0200

    avancement page de profile

--- Pawmunity/src/views/ProfilView.vue
+++ Pawmunity/src/views/ProfilView.vue
@@ -1,9 +1,26 @@
 <script setup>
 
+import { ref } from "vue";
+import { profils } from "../data/profils.js";
+
+import Banner from '@/components/Banner.vue'
+import ProfilBanner from '@/components/ProfilBanner.vue'
+import MenuBurger from '@/components/MenuBurger.vue'
+
+const profil =ref(profils[0])
 </script>
 
 <template>
-  <div>Profile</div>
+  <div class="min-h-screen bg-[#F5F1EB] flex flex-col">
+    <Banner />
+    <MenuBurger />
+    <ProfilBanner
+      :famille="profil.famille"
+      :nom="profil.nom"
+    />
+
+  </div>
+
 </template>
 
 <style scoped>
