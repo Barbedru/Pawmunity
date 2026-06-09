d9a890f25d759396c71dd5657a9cfd937ab19d2c
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-09 15:05:34 +0200

    correction

--- Pawmunity/src/views/LandingView.vue
+++ Pawmunity/src/views/LandingView.vue
@@ -27,7 +27,7 @@ const router = useRouter()
       <p class="text-center text-[#2C4A6E] text-base leading-relaxed mb-10">
         Connecte les familles <br>
         de ta communauté pour prendre soin<br>
-        de vos animals ensemble
+        de vos animaux ensemble
       </p>
 
       <!-- Créer un compte -->
