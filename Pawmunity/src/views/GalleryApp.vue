8e345a96285f41849beaedd0e437387b89a3bcf8
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-06-09 15:05:07 +0200

    Ajout page vitrine

---
+++ Pawmunity/src/views/GalleryApp.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#F5F1EB] flex flex-col">
+
+    <div class="flex justify-center">
+      <img
+        src="../assets/LogoApp.png"
+        alt="Illustration Pawmunity"
+        class="w-90 mb-8"
+      />
+    </div>
+
+    <div class="text-4xl text-gray-500 text-center bg-[#FF7A4D]  w-[500px] mx-auto h-[30px] rounded-full px-4 py-1 text-[20px]">
+      <p>Une communauté bienveillante pour vos animaux</p>
+    </div>
+
+    <div class="text-[#2C4A6E] font-bold text-center text-[60px] ">
+      <h1>Faites garder vos animaux <br> en toute confiance</h1>
+    </div>
+
+    <div class="text-center text-gray-500 text-[20px]">
+      <p> Organisez facilement la garde de vos compagnons entre familles de votre communauté.
+        Entraide, simplicité et tranquillité d'esprit.</p>
+    </div>
+
+    <div class="flex justify-center">
+      <img
+        src="../assets/qrcode.svg"
+        alt="QRcode"
+        class="w-90 mb-8 items-center justify-center px-6"
+      />
+    </div>
+
+    <div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
